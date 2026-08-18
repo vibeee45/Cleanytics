c97406cf557216ef27306ed02e9051443d497b5d
Vansh Bhardwaj <bhardwajvansh2501@gmail.com>
2026-08-18 10:58:53 +0530

    Adding Database config in the Project

--- Backend/app/core/config.py
+++ Backend/app/core/config.py
@@ -1,4 +1,9 @@
+from pathlib import Path
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+BASE_DIR = Path(__file__).resolve().parents[2]
 
 
 class Settings(BaseSettings):
@@ -7,13 +12,11 @@ class Settings(BaseSettings):
     DEBUG: bool = True
 
     DATABASE_URL: str
-
     REDIS_URL: str = "redis://localhost:6379/0"
-
     SECRET_KEY: str
 
     model_config = SettingsConfigDict(
-        env_file=".env",
+        env_file=BASE_DIR / ".env",
         env_file_encoding="utf-8",
         case_sensitive=True,
         extra="ignore",
